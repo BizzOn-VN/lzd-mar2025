987f0b02a216874e856cbdf6173bba487adeb53f
chikien111 <38467294+chikien111@users.noreply.github.com>
2025-03-18 10:14:31 +0700

    update page

--- scripts/main.js
+++ scripts/main.js
@@ -26,3 +26,23 @@ jQuery(document).on("ready",function () {
 
 // $("#md-popup-3").fancybox().trigger('click');
 // $("#md-popup-4").fancybox().trigger('click');
+
+
+const copySpan = document.getElementById('copySpan');
+const tooltipSpan = document.getElementById('tooltipac');
+  copySpan.addEventListener('click', function() {
+    const text = copySpan.textContent;
+
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        console.log('Text copied to clipboard:', text);
+        
+      })
+      .catch(err => {
+        console.error('Could not copy text: ', err);        
+      });
+      tooltipSpan.classList.add('active-tt');
+      setTimeout(function() {
+         tooltipSpan.classList.remove('active-tt');
+       }, 1500);
+  });
